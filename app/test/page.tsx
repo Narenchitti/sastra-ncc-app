8e565938d212286c18398ca250c3a5b921a19965
Narenchitti <naren.c8105@gmail.com>
2026-02-01 21:03:46 +0530

    Debug: Add Test Route and cleanup

---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+    return (
+        <div className="p-10 bg-green-100 text-green-800 font-bold text-2xl">
+            Routing System Check: PROTECTED_ROUTE_ACTIVE
+        </div>
+    )
+}
